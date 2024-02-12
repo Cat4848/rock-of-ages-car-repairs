93ae94e0a5fdf57a945464bc8285e26d4b8190ab
Catalin Pirtiu <102763743+Cat4848@users.noreply.github.com>
2024-02-12 21:42:43 +0000

    feat/navbar (#4)
    * feat/navbar-initial-setup
    
    * chore/eslint-setup (#5)
    
    * chore:installed-prettier
    
    * chore:changed-eslint-config-filename
    
    * chore:eslint-config-completed
    
    * feat:navbar-initial-setup
    
    * feat:added-link-com[
    
    * feat:nav-bar-added
    
    * feat:navbar-complete

---
+++ app/Components/NavBar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import styled from "styled-components";
+import LinkWrapper from "./LinkWrapper";
+
+const Wrapper = styled.div`
+  background: black;
+  color: gold;
+  padding: 1vmin 1 vmin;
+  height: 10em;
+`;
+
+const Links = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: calc(1vw + 2lh);
+  justify-items: center;
+  justify-content: space-between;
+  margin: 0;
+  padding: 2vmin;
+  padding-bottom: 0.2lh;
+  font-size: calc(0.7rem + 1vw);
+`;
+
+interface INavBarItem {
+  href: string;
+  linkName: string;
+}
+
+interface INavBar {
+  navbarItems?: INavBarItem[];
+  logo?: React.ReactNode | string;
+}
+
+export default function NavBar({
+  navbarItems = [],
+  logo = "Your Logo Here"
+}: INavBar) {
+  return (
+    <Wrapper>
+      <Links>
+        <LinkWrapper href="/">{logo}</LinkWrapper>
+        {navbarItems.map((item) => (
+          <LinkWrapper href={`/${item.href}`} key={item.href}>
+            {item.linkName}
+          </LinkWrapper>
+        ))}
+      </Links>
+    </Wrapper>
+  );
+}
