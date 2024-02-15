24795b7fd569ead0b3887a128e2b5d13508f711d
Cat4848 <catalinrol@gmail.com>
2024-02-15 16:50:09 +0000

    feat:added-values-object

--- components/Values.tsx
+++ components/Values.tsx
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const values = [{
+  name: 'Honest Mechanic',
+  icon: 
+}]
+
 
 export default function Values(){
   return (
