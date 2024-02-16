f3ec7fe64690d9a9f055380c6d8887dd12085924
Cat4848 <catalinrol@gmail.com>
2024-02-16 12:04:34 +0000

    refactor:renamed-import-CarServices-component-

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import styled from "styled-components";
 import yellowCar from "../assets/yellow-toy-car-in-dramatic-desert-landscape.svg";
 import MainImage from "@/components/MainImage";
-import Services from "@/components/services/CarServices";
+import CarServices from "@/components/services/CarServices";
 import Values from "@/components/Values";
 
 const Wrapper = styled.div`
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <Wrapper>
       <MainImage src={yellowCar} alt={mainImageAlt} text={mainImageText} />
-      <Services />
+      <CarServices />
       <Values />
     </Wrapper>
   );
