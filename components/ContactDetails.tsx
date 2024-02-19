a722560b4d73292dcb2a95ffcc4375a8416f613a
Cat4848 <catalinrol@gmail.com>
2024-02-19 17:58:02 +0000

    feat/added-mobile-number

--- components/ContactDetails.tsx
+++ components/ContactDetails.tsx
@@ -1,1 +1,25 @@
 import styled from "styled-components";
+
+interface IContactDetails {
+  business: string;
+  tel: string;
+  email: string;
+}
+
+const Wrapper = styled.div``;
+
+export default function ContactDetails({
+  business = "",
+  tel = "",
+  email = ""
+}: IContactDetails) {
+  return (
+    <Wrapper itemScope itemType="https://schema.org/LocalBusiness">
+      <h1 itemProp="name">{business}</h1>
+      Touch Call:
+      <span itemProp="telephone">
+        <a href={tel}>{tel}</a>
+      </span>
+    </Wrapper>
+  );
+}
