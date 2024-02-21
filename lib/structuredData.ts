a99cbdccc1bdfe74dfe978e6b9129732b4f0bae0
Cat4848 <catalinrol@gmail.com>
2024-02-21 20:27:28 +0000

    feat:added-structured-data-file

---
+++ lib/structuredData.ts
@@ -0,0 +1,38 @@
+import { Organization, Thing, WithContext, ImageObject } from "schema-dts";
+
+export const business = {
+  type: "Mobile Mechanic",
+  countryCode: "+44",
+  mobile: "7845465922",
+  email: "catalinrol@gmail.com"
+};
+
+function jsonLd<T extends Thing>(data: WithContext<T>) {
+  return `
+    ${JSON.stringify(data)}
+  `;
+}
+
+const image1: WithContext<ImageObject> = {
+  "@context": "https://schema.org",
+  "@type": "ImageObject",
+  author: "Rock of Ages Auto Repairs",
+  name: "Mobile Mechanic Kent",
+  contentUrl: "yellow-toy-car-in-dramatic-desert-landscape.jpeg",
+  description: "Mobile Mechanic Services Presentation"
+};
+
+export const organization = jsonLd<Organization>({
+  "@context": "https://schema.org",
+  "@type": "AutoRepair",
+  image: [image1],
+  name: "Mobile Mechanic in Kent",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "Sunderland Close",
+    postalCode: "ME1 3AS",
+    addressRegion: "England",
+    addressLocality: "Kent",
+    addressCountry: "GB"
+  }
+});
