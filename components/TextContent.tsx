604e8316ea8e0d15255f2824b634f40973f1e875
Cat4848 <catalinrol@gmail.com>
2024-02-22 14:37:26 +0000

    style:added-content-style

--- components/TextContent.tsx
+++ components/TextContent.tsx
@@ -1,7 +1,17 @@
 import styled from "styled-components";
+import color from "@/lib/color";
 
 const Wrapper = styled.div``;
-const Paragraph = styled.p``;
+const Paragraph = styled.p`
+  margin: 2em 0;
+  padding: 2em;
+  font-size: 1.4em;
+  text-align: justify;
+  background: black;
+  opacity: 0.75;
+  color: ${color.silver};
+  width: 90vw;
+`;
 
 interface ITextContent {
   text: string;
