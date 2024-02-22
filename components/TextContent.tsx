e4addb0ce67a460b70cbb242fa300e30793510f2
Cat4848 <catalinrol@gmail.com>
2024-02-22 13:44:00 +0000

    feat:added-TextComponent-that-generates-paragraphs

--- components/TextContent.tsx
+++ components/TextContent.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div``;
+const Paragraph = styled.p``;
 
-export default function TextContext({text}) {
+interface ITextContent {
+  text: string;
+}
+export default function TextContext({ text }: ITextContent) {
+  const content = text.split("\n");
   return (
-    <Wrapper></Wrapper>
-  )
+    <Wrapper>
+      {content.map((paragraph, i) => {
+        if (!paragraph) return;
+        return <Paragraph key={i}>{paragraph}</Paragraph>;
+      })}
+    </Wrapper>
+  );
 }
