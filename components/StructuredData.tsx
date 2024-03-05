01ac47be54a929d958183dbf27854d9c3c10fcfc
Cat4848 <catalinrol@gmail.com>
2024-03-05 18:49:26 +0000

    chore:changed-structured-data-location

--- components/StructuredData.tsx
+++ components/StructuredData.tsx
@@ -1,17 +1,13 @@
-import Head from "next/head";
-
 interface IStructuredData {
   data: string;
 }
 
 export default function StructuredData({ data }: IStructuredData) {
   return (
-    <Head>
-      <script
-        key="structured-data"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: data }}
-      />
-    </Head>
+    <script
+      key="structured-data"
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: data }}
+    />
   );
 }
